93b7bb9aa63f17cd410b6c77d41d69278226db9b
Francois Le Pape <francois@ninetailed.com>
2024-03-10 23:25:26 +0100

    add GITHUB_COM_TOKEN

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  branchPrefix: "renovate/",
+  username: "renovate-release",
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  onboarding: false,
   platform: "github",
+  repositories: ["Lp-Francois/test-helm-versioning"],
 };
